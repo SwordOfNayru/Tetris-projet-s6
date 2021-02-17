94cb1ce20909742bed399123a05d77f7ea44ec45
Pierre-Louis Berquez <plb.etud@gmail.com>
2021-02-17 12:25:27 +0100

    rajout proprieté + commentaire piece

--- src/class/Pieces.js
+++ src/class/Pieces.js
@@ -1,15 +1,59 @@
 class Piece {
-    partern = [];
-    pos = {'x':0.0, 'y':0.0 };
+    partern = []; //L'ensemble des paternes d'une piece ==> this.partern[rotation][ligne][colonne]
+    pos = {'x':0.0, 'y':0.0 }; //Soit le coin superieur haut gauche
+                               // on définis 2 position la position réel soit la position analogique de la pièce
+                               // et la position absolue celle d'une case de la matrice.
+                               // pour définir l'emplacement d'affichage : 
+                               //    Absolue ==> Réel ==> Affichage
+    rotation = 0; //La rotation va de 0 a 3. 0 est la position initial 3 celle après 3 rotation vers la droite
+    color = "FFFFFF"; //Couleur de la piece
+    
     constructor() {
 
     }
+
+    //Les fonction turn permet de changer le parterne de rotation il ne verifie par si la rotation est possible.
+    turnPieceRight() {
+        if(this.rotation == 3) {
+            this.rotation = 0;
+        }
+        else {
+            this.rotation++;
+        }
+        return this.rotation;
+    }
+    turnPieceLeft() {
+        if(this.rotation == 0) {
+            this.rotation = 3;
+        }
+        else {
+            this.rotation--;
+        }
+        return this.rotation;
+    }
+
+    getPosBlock() { //Renvoie la postion individuel des blocs en position réèl
+        let bloc = [];
+        for (let iRow = 0; iRow < 4; iRow++) {
+            for (let iCol = 0; iCol < 4; iCol++) {
+                if(this.partern[this.rotation][iRow][iCol]) {
+                    bloc.push({
+                        'x': (this.pos['x'] + iCol),
+                        'y': (this.pos['y'] + iRow),
+                    });
+                }  
+            }
+        }
+        return bloc;
+    }
+
 }
 
 
 class O extends Piece {
     constructor() {
         super();
+        this.color = "FFEC00";
         this.partern = [
             [
                 [false,false,false,false],
@@ -43,6 +87,7 @@ class O extends Piece {
 class I extends Piece {
     constructor() {
         super();
+        this.color = "009EE2";
         this.partern = [
             [
                 [false,false,false,false],
@@ -75,6 +120,7 @@ class I extends Piece {
 class T extends Piece {
     constructor() {
         super();
+        this.color ="662482";
         this.partern = [
             [
                 [false,false,false,false],
@@ -107,6 +153,7 @@ class T extends Piece {
 class L extends Piece {
     constructor() {
         super();
+        this.color = "F29100";
         this.partern = [
             [
                 [false,false,false,false],
@@ -139,6 +186,7 @@ class L extends Piece {
 class J extends Piece {
     constructor() {
         super();
+        this.color = "312782";
         this.partern = [
             [
                 [false,false,false,false],
@@ -171,6 +219,7 @@ class J extends Piece {
 class S extends Piece {
     constructor() {
         super();
+        this.color = "009540";
         this.partern = [
             [
                 [false,false,false,false],
@@ -203,6 +252,7 @@ class S extends Piece {
 class Z extends Piece {
     constructor() {
         super();
+        this.color = "E20613"
         this.partern = [
             [
                 [false,false,false,false],
@@ -232,8 +282,9 @@ class Z extends Piece {
     }
 }
 
+//Class pour encapsuler la génération de piece pour eviter les problèmes de dépendance.
 class PieceGenerator {
-    static PieceArray = [I,O,T,S,Z,L,J];
+    static PieceArray = [I,O,T,S,Z,L,J]; //On y stock les descriptions de class
 
     static generatePiece() {
         return new this.PieceArray[this.getRandomInt()]();
