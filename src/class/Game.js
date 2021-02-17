12e7b2bd6ad164c856d0c8f935bab878c286f3ee
Pierre-Louis Berquez <plb.etud@gmail.com>
2021-02-17 12:29:07 +0100

    commentaire et préparation class joueur

---
+++ src/class/Game.js
@@ -0,0 +1,9 @@
+
+//Equivalent de la class système. 
+//
+//Avoir le jeu encapsuler dans une class permet d'en lancé plusieurs.
+//TODO : Doit pouvoir fonctionner sans interface graphique.
+class Game {
+
+
+}
