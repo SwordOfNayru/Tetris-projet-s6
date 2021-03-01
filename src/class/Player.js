1a4196dc34561da28af71f73807e500189813e77
SwordOfNayru <44189025+SwordOfNayru@users.noreply.github.com>
2021-03-01 11:38:46 +0100

    Delete Player.js

--- src/class/Player.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Player = void 0;
-var Player = /** @class */ (function () {
-    function Player() {
-    }
-    return Player;
-}());
-exports.Player = Player;
